0c8d0c4e111ddc7dccadd2fd7a90fff0d18cbe9d
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-03-10 19:58:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const attendants= ['John','Marry','Ali','Peter'];
+const event='Birthday'
+function writeCards(attendants,event){
+    for(i=0;i<attendants.length;i++){
+        console.log(`Thank you,${attendants[i]}, for the wonderful ${event} gift`);
+    }
+
+    return(attendants);
+    
+}
+console.log(writeCards(attendants))
+
+function countDown(){
+    let countup = 15;
+    while (countup > 0) {
+      console.log(countup--);
+    }
+
+    return countDown;
+}
+console.log(countDown());
